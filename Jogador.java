0b666350abda6a9b17bbee94d0b82696b108baa4
Henrique Fedalto <hiqmiguel4@gmail.com>
2025-04-16 20:13:34 -0300

    Update Jogador.java

--- Jogador.java
+++ Jogador.java
@@ -1,5 +1,5 @@
 /**
- * A classe Jogador representa o jogador do jogo, contem nala as informações
+ * A classe Jogador representa um jogador de um jogo, contendo informações
  * sobre seu nome, número de tentativas e pontuação.
  */
 public class Jogador {
@@ -10,12 +10,12 @@ public class Jogador {
     private String nome;
     
     /** 
-    * Número de tentativas 
+    * Número de tentativas feitas pelo jogador 
     */
     private int tentativas;
     
     /** 
-    * Pontuação atual 
+    * Pontuação atual do jogador 
     */
     private int pontuacao;
 
@@ -40,7 +40,7 @@ public class Jogador {
     }
 
     /**
-     * Definição do nome do jogador.
+     * Define o nome do jogador.
      *
      * @param nome O novo nome do jogador.
      */
@@ -80,9 +80,23 @@ public class Jogador {
      *
      * @param pontuacao A nova pontuação.
      */
-    public void set
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
 
+    /**
+     * Atualiza a pontuação do jogador somando o valor informado.
+     *
+     * @param valor Valor a ser adicionado à pontuação.
+     */
+    public void atualizarPontuacao(int valor) {
+        this.pontuacao += valor;
+    }
+
+    /**
+     * Incrementa o número de tentativas do jogador em 1.
+     */
     public void incrementarTentativas() {
-        this.tentativas ++;
+        this.tentativas++;
     }
 }
